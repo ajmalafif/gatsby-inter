0a0ec875331fad801ed88b4c7667b3ce1baeffa6
Ajmal Afif <ajmalafif@gmail.com>
2018-08-13 21:37:46 +0800

    Document.write?

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,58 +13,58 @@ module.exports = {
     //     }
     //   }
     // },
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-catch-links',
-    {
-      resolve: 'gatsby-plugin-favicon',
-      options: {
-        logo: './src/favicon.png',
-        injectHTML: true,
-        icons: {
-          android: false,
-          appleIcon: true,
-          appleStartup: false,
-          coast: true,
-          favicons: true,
-          firefox: false,
-          twitter: false,
-          yandex: false,
-          windows: true
-        }
-      }
-    },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'css',
-        path: `${__dirname}/src/css/`,
-      },
-    },
-    {
-      resolve: 'gatsby-plugin-sitemap'
-    },
-    {
-      resolve: 'gatsby-plugin-netlify',
-      options: {
-        headers: {
-          '/static/*': [
-            'Cache-Control: public, max-age=16070400'
-          ]
-        }
-      }
-    },
-    {
-      resolve: 'gatsby-plugin-manifest',
-      options: {
-        name: 'ajmalafif',
-        short_name: 'ajmalafif',
-        start_url: '/',
-        background_color: '#fff',
-        theme_color: '#05f',
-        display: 'minimal-ui',
-        icon: 'src/images/icon.png', // This path is relative to the root of the site.
-      }
-    },
-    'gatsby-plugin-offline'
+    'gatsby-plugin-react-helmet'
+    // 'gatsby-plugin-catch-links',
+    // {
+    //   resolve: 'gatsby-plugin-favicon',
+    //   options: {
+    //     logo: './src/favicon.png',
+    //     injectHTML: true,
+    //     icons: {
+    //       android: false,
+    //       appleIcon: true,
+    //       appleStartup: false,
+    //       coast: true,
+    //       favicons: true,
+    //       firefox: false,
+    //       twitter: false,
+    //       yandex: false,
+    //       windows: true
+    //     }
+    //   }
+    // },
+    // {
+    //   resolve: 'gatsby-source-filesystem',
+    //   options: {
+    //     name: 'css',
+    //     path: `${__dirname}/src/css/`,
+    //   },
+    // },
+    // {
+    //   resolve: 'gatsby-plugin-sitemap'
+    // },
+    // {
+    //   resolve: 'gatsby-plugin-netlify',
+    //   options: {
+    //     headers: {
+    //       '/static/*': [
+    //         'Cache-Control: public, max-age=16070400'
+    //       ]
+    //     }
+    //   }
+    // },
+    // {
+    //   resolve: 'gatsby-plugin-manifest',
+    //   options: {
+    //     name: 'ajmalafif',
+    //     short_name: 'ajmalafif',
+    //     start_url: '/',
+    //     background_color: '#fff',
+    //     theme_color: '#05f',
+    //     display: 'minimal-ui',
+    //     icon: 'src/images/icon.png', // This path is relative to the root of the site.
+    //   }
+    // },
+    // 'gatsby-plugin-offline'
   ]
 }
